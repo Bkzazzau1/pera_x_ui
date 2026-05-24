6710a1547c78327f938aa821f5c172608fb10583
Bkzazzau1 <ibrahim.yahaya@miva.edu.ng>
2026-05-24 17:19:24 +0100

    Add checkout payment method state

--- lib/app/state/checkout_provider.dart
+++ lib/app/state/checkout_provider.dart
@@ -1,14 +1,29 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod/legacy.dart';
 
-final payWithPexProvider = StateProvider<bool>((ref) => false);
+import '../../features/checkout/models/payment_method.dart';
+
+final checkoutCountryCodeProvider = StateProvider<String>((ref) => 'NG');
+
+final selectedPaymentMethodProvider = StateProvider<PaymentMethodType>(
+  (ref) => PaymentMethodType.pexToken,
+);
+
+final availablePaymentMethodsProvider = Provider<List<PaymentMethodType>>((ref) {
+  final countryCode = ref.watch(checkoutCountryCodeProvider);
+  return availablePaymentMethods(countryCode: countryCode);
+});
+
+final payWithPexProvider = Provider<bool>((ref) {
+  return ref.watch(selectedPaymentMethodProvider) == PaymentMethodType.pexToken;
+});
 
 final checkoutShippingProvider = Provider<double>((ref) => 5.00);
 
 final checkoutDiscountProvider = Provider<double>((ref) {
-  final payWithPex = ref.watch(payWithPexProvider);
+  final paymentMethod = ref.watch(selectedPaymentMethodProvider);
 
-  if (!payWithPex) return 0;
+  if (!paymentMethod.earnsPexDiscount) return 0;
 
   return 0.10;
 });
