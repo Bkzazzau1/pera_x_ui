a883d779a3dbc03025acd1b69d0fdb40fdb3bf11
Bkzazzau1 <ibrahim.yahaya@miva.edu.ng>
2026-05-26 11:19:55 +0100

    Register frontend pricing providers

--- lib/app/state/service_providers.dart
+++ lib/app/state/service_providers.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../features/ai_lab/data/ai_service.dart';
 import '../../features/checkout/data/checkout_service.dart';
 import '../../features/market/data/product_service.dart';
+import '../../features/pricing/data/pricing_service.dart';
 import '../../features/wallet/data/wallet_service.dart';
 
 final walletServiceProvider = Provider<WalletService>((ref) {
@@ -15,6 +16,18 @@ final productServiceProvider = Provider<ProductService>((ref) {
 
 final aiServiceProvider = Provider<AiService>((ref) {
   return AiService();
+});
+
+final pricingServiceProvider = Provider<PricingService>((ref) {
+  return PricingService();
+});
+
+final utilityPricingProvider = FutureProvider<List<UtilityPriceModel>>((ref) {
+  return ref.read(pricingServiceProvider).getUtilityPricing();
+});
+
+final creditExchangeRatesProvider = FutureProvider<List<CreditExchangeRateModel>>((ref) {
+  return ref.read(pricingServiceProvider).getCreditExchangeRates();
 });
 
 final checkoutServiceProvider = Provider<CheckoutService>((ref) {
